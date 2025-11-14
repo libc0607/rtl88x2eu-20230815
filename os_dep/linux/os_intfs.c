ea52a3962873b117e5e03eb00bc7e177762d30d3
Ruixi Zhou <zhouruixi@gmail.com>
2025-11-15 00:01:03 +0800

    Fix VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver not import error
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -25,6 +25,7 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Realtek Wireless Lan Driver");
 MODULE_AUTHOR("Realtek Semiconductor Corp.");
 MODULE_VERSION(DRIVERVERSION);
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
 MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
 #endif
